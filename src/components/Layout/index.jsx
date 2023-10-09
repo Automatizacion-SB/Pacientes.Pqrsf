b5e6a8a4f5f152929b4bfe52a290dd43dd8ce9bf
Practicante Sistemas <psistemas@hisantaana.com>
2023-10-09 11:05:56 -0500

    fix: :iphone: response Layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,7 +1,11 @@
 import "./style.css";
 /* eslint-disable react/prop-types */
 function Layout({ children }) {
-  return <div className="container">{children}</div>;
+  return (
+    <div className="container">
+      <div className="container-page">{children}</div>;
+    </div>
+  );
 }
 
 export { Layout };
